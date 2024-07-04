d1eb0d9f6823dab6219cf8c133fb3c41240a7d83
Vicente Pastor <github@pustalorc.com>
2024-07-04 12:16:19 +0200

    Fix exception message

--- ERC721/src/types/exceptions/ContractMethodNotFound.exception.ts
+++ ERC721/src/types/exceptions/ContractMethodNotFound.exception.ts
@@ -1,10 +1,10 @@
 import AppError from "./AppError.exception";
 
 export default class ContractMethodNotFoundException extends AppError {
-  constructor(contractName: string, methodName: string, contractAddress: string, contractMethods: string[]) {
+  constructor(contractName: string, contractAddress: string, methodName: string, contractMethods: string[]) {
     super(
       'ContractMethodNotFoundException', 
-      `Contract '${contractName}' does not have a method '${methodName}'`,
+      `Contract '${contractName}' does not have a method '${methodName}', or it doesn't have a method '${methodName}' that accepts the provided arguments.`,
       { contract: {
         name: contractName,
         address: contractAddress,
